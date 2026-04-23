a4c1226a19396486139d54138e472cb3bcc3a014
水灵 <1668589925@qq.com>
2026-04-23 21:48:58 +0800

    a

--- src/config.ts
+++ src/config.ts
@@ -46,7 +46,7 @@ export const siteConfig: SiteConfig = {
 		projects: false, // 项目页面开关
 		skills: false, // 技能页面开关
 		timeline: false, // 时间线页面开关
-		albums: false, // 相册页面开关
+		albums: true, // 相册页面开关
 		devices: false, // 设备页面开关
 	},
 
